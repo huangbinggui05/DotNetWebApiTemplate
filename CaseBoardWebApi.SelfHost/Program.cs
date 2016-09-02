5bed0314ae68b0e420b7f1382b9d11bf934265a4
huangbinggui05 <huangbinggui05@qq.com>
2016-09-02 09:58:31 +0800

    CaseBoardWebApi.SelfHost

---
+++ CaseBoardWebApi.SelfHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CaseBoardWebApi.SelfHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8006");
+            WebApiConfiguration.Configure(config);
+            var host = new HttpSelfHostServer(config);
+            host.OpenAsync().Wait();
+            Console.WriteLine("IssueApi hosted at: {0}", config.BaseAddress);
+            Console.ReadLine();
+        }
+    }
+}
