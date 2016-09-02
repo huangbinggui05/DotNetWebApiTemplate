41a4cd705af6cb3c9333988f4ea8a3c35befe7f3
huangbinggui05 <huangbinggui05@qq.com>
2016-09-02 09:49:53 +0800

    upload test

---
+++ Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.async_await;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = (new MyClass()).DoWorkAsync();
+            t.Wait();
+        }
+    }
+}
---
+++ Tests/async-await/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.async_await
+{
+    class MyClass
+    {
+        public int Get10()
+        {
+            return 10;
+        }
+        public async Task DoWorkAsync()
+        {
+            Func<int> ten = new Func<int>(Get10);
+            int a = await Task.Run(ten);
+            int b = await Task.Run(new Func<int>(Get10));
+            int c = await Task.Run(() => { return 10; });
+        }
+    }
+}
